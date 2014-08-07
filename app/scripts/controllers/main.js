9986c0f0f66ee2716ed015859a66100b60aaf36f
Devyn Stott <devyn.stott@intel.com>
2014-08-06 22:55:16 -0700

    Homepage cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,9 @@
 'use strict';
 
 angular.module('chemicalNamesApp')
-  .controller('MainCtrl', function ($scope, anionFactory, cationFactory, compoundFactory) {
-        var init = function() {
-            $scope.compound = $scope.getRandomCompound();
-            console.log($scope.compound);
-        };
-
-        $scope.getRandomCompound = function() {
-            return compoundFactory.getChemicalCompound(cationFactory.randomCation(), anionFactory.randomAnion());
+    .controller('MainCtrl', function ($scope, anionFactory, cationFactory, compoundFactory) {
+        var init = function () {
         };
 
         init();
-  });
+    });
