5056832cb7b64a9a1f0fee5bd1fd9cfc46e2e5cc
Devyn Stott <devyn.stott@intel.com>
2014-08-07 11:53:40 -0700

    Stub out test comparision. needs more work and styling

--- app/scripts/controllers/test-yourself.js
+++ app/scripts/controllers/test-yourself.js
@@ -1,10 +1,28 @@
 'use strict';
 
 angular.module('chemicalNamesApp')
-    .controller('TestYourselfCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+    .controller('TestYourselfCtrl', function ($scope, anionFactory, cationFactory, compoundFactory) {
+        var init = function () {
+            $scope.getRandomCompound();
+            console.log($scope.compound);
+            $scope.userInput = '';
+            $scope.answerSubmitted = false;
+            $scope.isCorrectAnswer = false;
+        };
+
+        $scope.testAnswer = function () {
+            $scope.answerSubmitted = true;
+            $scope.isCorrectAnswer = $scope.compound.formula == $scope.userInput;
+        };
+
+        $scope.getRandomCompound = function () {
+            $scope.answerSubmitted = false;
+            $scope.compound = compoundFactory.getChemicalCompound(cationFactory.randomCation(), anionFactory.randomAnion());
+        };
+
+        $scope.addSubscript = function (number) {
+            $scope.userInput += compoundFactory.getSubscriptFor(number);
+        };
+
+        init();
     });
