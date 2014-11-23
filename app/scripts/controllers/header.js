5e441f073d73da65672e4a44b62ac9d281757128
Devyn Stott <devyn.stott@intel.com>
2014-11-23 15:55:48 -0800

    remove dist folder

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -1,32 +1,33 @@
 'use strict';
 
 angular.module('chemicalNamesApp')
-    .controller('HeaderCtrl', function ($scope, $location) {
-        $scope.root = "/#";
-        $scope.links = [
-            {
-                name: 'Home',
-                link: '/'
-            },
-            {
-                name: 'Flashcards',
-                link: '/flashcards'
-            },
-            {
-                name: 'Quiz',
-                link: '/quiz-yourself'
-            },
-            {
-                name: 'About',
-                link: '/about'
-            }
-        ];
+  .controller('HeaderCtrl', function ($scope, $location) {
+    //$scope.root = "/#"; //for local development
+    $scope.root = "ChemicalNames/#"; //for gh-pages deployment
+    $scope.links = [
+      {
+        name: 'Home',
+        link: '/'
+      },
+      {
+        name: 'Flashcards',
+        link: '/flashcards'
+      },
+      {
+        name: 'Quiz',
+        link: '/quiz-yourself'
+      },
+      {
+        name: 'About',
+        link: '/about'
+      }
+    ];
 
-        $scope.$on('$locationChangeSuccess', function () {
-            var thisLink = $location.path();
-            angular.forEach($scope.links, function (link) {
-                link.active = link.link == thisLink;
-            });
-        });
-
+    $scope.$on('$locationChangeSuccess', function () {
+      var thisLink = $location.path();
+      angular.forEach($scope.links, function (link) {
+        link.active = link.link == thisLink;
+      });
     });
+
+  });
